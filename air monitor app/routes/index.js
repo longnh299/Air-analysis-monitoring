4109998e83762069fef58d759388b75a4985b2cc
Long Nguyen <longkakapro2000@gmail.com>
2022-01-24 16:05:44 +0700

    update index.js

--- air monitor app/routes/index.js
+++ air monitor app/routes/index.js
@@ -7,6 +7,7 @@ const { ensureAuthenticated, forwardAuthenticated } = require('../config/auth');
 // Welcome Page
 router.get('/', forwardAuthenticated, (req, res) => res.render('welcome'));
 
+
 // Dashboard
 
 router.get('/dashboard', ensureAuthenticated, (req, res) =>
